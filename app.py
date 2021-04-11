06b5cdd3f0f84b7c24dfd87247c9d3e169a19249
Manpreet Kaur <51187449+Manpreet1377@users.noreply.github.com>
2021-04-11 16:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,29 +27,28 @@ def clean_message(text):
 with open('senti.pkl', "rb") as f:
     senti = pickle.load(f)
     
-# One hot encoding
-with open('one_hot.pkl', "rb") as f:
+#One hot encoding
+with open('encoding.pkl', "rb") as f:
     enc = pickle.load(f)
     
-# for model Prediction
+#for model Prediction
 xgb_b=xgboost.Booster()
-xgb_b.load_model('xgbr.booster')
+xgb_b.load_model('xgb.booster')
 
 
 # To get information for the Reddit url
 def extract_data(url):
     data = {}
-    reddit = praw.Reddit(client_id='WUTH6H3Cx7KW4w',
-                         client_secret='hBOWXZ37WOOY9M9oT-SD-2H7ql_7HQ',
-                         user_agent='user_agent')
+    reddit = praw.Reddit(client_id='4T9G7xV5eOUCSg',
+                         client_secret='FhsUszOtYyW7IM3XpD7n9FavD3EFaQ',
+                         user_agent='Preet')
     sub_data = reddit.submission(url=str(url))
-    data['body'] = [str(sub_data.title)]
-    data['downs'] = sub_data.downs
+    data['Title'] = [str(sub_data.title)]
     data['upvote_ratio']=sub_data.upvote_ratio
-    data['gilded'] = [sub_data.gilded]
+    data['Gilded'] = [sub_data.gilded]
     data['word_count'] = word_count(sub_data.title)
-    data['over_18'] = [sub_data.over_18]
-    data['number_of_Comments'] = [sub_data.num_comments]
+    data['Over_18'] = [sub_data.over_18]
+    data['Number_of_Comments'] = [sub_data.num_comments]
     data['Subjectivity'] = Subjectivity(sub_data.title)
     data['Polarity'] = Polarity(sub_data.title)
     scores = senti.polarity_scores(sub_data.title)
@@ -58,29 +57,28 @@ def extract_data(url):
     data['neu'] = scores['neu']
     data['pos'] = scores['pos']
     df = pd.DataFrame(data)
-        
     return df
 
 @app.route('/')
 def home():
-    return render_template('Index.html')
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     url = str(flask.request.form['url'])
     data = extract_data(url)
-    Body = clean_message(data['body'][0])
+    Title = clean_message(data['Title'][0])
     # Converting word2vector
-    df_word_token = pd.read_csv('word_token.csv')
+    df_word_token = pd.read_csv('word_token_final.csv')
     print(' df_word_token loaded')
     sys.stdout.flush()
     test_title = []
-    for word in Body.split():
+    for word in Title.split():
         if word in df_word_token.columns:
             test_title.append(df_word_token[word])
     max_len = 300
     test_title = test_title + [0] * (max_len - len(test_title))
-    embed_mat = np.array(pd.read_csv('embed_mat.csv', sep=' '))
+    embed_mat = np.array(pd.read_csv('embedded_final.csv', sep=' '))
     vectors = []
     for n in test_title:
         vectors.append(embed_mat[n])
@@ -89,20 +87,21 @@ def predict():
     final_vector = np.mean(arr, axis=0)
     df_test_body = pd.DataFrame(np.array(final_vector)).T
     
-    # one hot encoding with column names
-    categories = ['over_18']
-    test_encoded = enc.transform(data[categories])  
+    #one hot encoding with column names
+    categories = ['Over_18']
+    test_encoded = enc.transform(data[categories])
     col_names = [False, True]
     test_ohe = pd.DataFrame(test_encoded.todense(), columns=col_names)
     
-    data.drop(["body", 'over_18'], axis=1, inplace=True)
+    
+    data.drop(["Title", 'Over_18'], axis=1, inplace=True)
     data.reset_index(inplace=True, drop=True)
     X_test = pd.concat([data, df_test_body, test_ohe], axis=1)
-
+    
+    
     #  Predict with XGBoosting Regressor
     score = xgb_b.predict(xgboost.DMatrix(X_test))
-    return render_template('Index.html', score='Predicted score for the given Reddit post is: {}'.format(score ))
-
+    return render_template('index.html', score='Predicted score for the given Reddit post is: {}'.format(score ))
 
 if __name__ == "__main__":
     app.run(debug=True)
